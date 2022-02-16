ef080c8f78ad84478f77727f233b2a15b5730629
Anna Shishkova <asshishkova@gmail.com>
2022-02-16 14:51:49 +0200

    list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,35 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
   def index
     @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'list was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
